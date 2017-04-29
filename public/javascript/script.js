5107bc26ce6a8fd1a2d2dd791dde895573456aea
venu7731 <venu.nalla45@gmail.com>
2017-04-28 22:48:11 -0500

    Ajax and json for

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -27,13 +27,100 @@ function materialsNeeded(){
 function showstuff(el){
     var elementId = document.getElementById(el);
     for(i=0; i<elementId.length; i++){
-    console.log(elementId[i].text);
-    console.log(elementId[i].value);
-    console.log(elementId[1].selected);
+    // console.log(elementId[i].text);
+    // console.log(elementId[i].value);
+    // console.log(elementId[i].selected);
+    if(elementId[i].selected=== true){
+        var x = elementId[i].text;
+    }
+    
 
 }
+return x;
 }
+function lighting(){
+    var elementId = document.getElementsByName("bulb");
+    var mycheck = "";
+    for(i=0;i<elementId.length;i++){
+// console.log(elementId.length);
+// console.log(elementId[i].checked)
+    if(elementId[i].checked=== true){
+        mycheck = mycheck + elementId[i].value + '<br>';
+    }
+    }
+return mycheck;
+    }
+
 function evaluatePage(){
-showstuff("home");
-showstuff("state");  
+    var feedbackHome = showstuff("home");
+    var feedbackState = showstuff("state");
+    var feedbackLight = lighting();
+    document.getElementById("output").innerHTML = feedbackHome + '<br>' + feedbackState + '<br>' + feedbackLight;  
 }
+
+function myfunction(){
+    var dateClicked = document.getElementById("buttonClicked"); 
+    var numbered = dateClicked.options[dateClicked.selectedIndex].text;
+document.getElementById("button").innerHTML = numbered;
+}
+// Ajax and json for animals
+var pageCounter = 1;
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
+  ourRequest.onload = function() {
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+    } else {
+      console.log("We connected to the server, but it returned an error.");
+    }
+    
+  };
+
+  ourRequest.onerror = function() {
+    console.log("Connection error");
+  };
+
+  ourRequest.send();
+  pageCounter++;
+  if (pageCounter > 3) {
+    btn.classList.add("hide-me");
+  }
+});
+
+function renderHTML(data) {
+  var htmlString = "";
+
+  for (i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+    
+    for (ii = 0; ii < data[i].foods.likes.length; ii++) {
+      if (ii == 0) {
+        htmlString += data[i].foods.likes[ii];
+      } else {
+        htmlString += " and " + data[i].foods.likes[ii];
+      }
+    }
+
+    htmlString += ' and dislikes ';
+
+    for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+      if (ii == 0) {
+        htmlString += data[i].foods.dislikes[ii];
+      } else {
+        htmlString += " and " + data[i].foods.dislikes[ii];
+      }
+    }
+
+    htmlString += '.</p>';
+
+  }
+
+  animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
+// end of Ajax and json function in javascript
